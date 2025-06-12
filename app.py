a6bbb3562f50daa8f9cba64f11505137a802568f
Alisson Abreu <alissonabreuh@gmail.com>
2025-06-11 22:33:40 -0300

    Create route logout

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, jsonify, request
 from models.user import User
 from database import db
-from flask_login import LoginManager, login_user, current_user
+from flask_login import LoginManager, login_user, current_user, logout_user, login_required
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "your_secret_key"
@@ -10,6 +10,11 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 login_manager = LoginManager()
 db.init_app(app)
 login_manager.init_app(app)
+login_manager.login_view = 'login'
+
+@login_manager.user_loader
+def  load_user(user_id):
+    return User.query.get(user_id)
 
 @app.route('/login', methods=["POST"])
 def login():
@@ -20,12 +25,18 @@ def login():
     if username and password:
          user = User.query.filter_by(username=username).first()
 
-    if user and user.password == password:
-        login_user(user)
-        return jsonify({"message": "Logado com sucesso"})
+         if user and user.password == password:
+             login_user(user)
+             return jsonify({"message": "Logado com sucesso"})
+         
     return jsonify({"message": "Credenciais inválidas"}), 400 
 
+@app.route('/logout', methods=["GET"])
+@login_required
+def logout():
+    logout_user()
+    return jsonify({"message": "Deslogado com sucesso."})
+
+
 if __name__ == '__main__':
-    with app.app_context():
-        db.create_all()  # Cria todas as tabelas definidas nos modelos
     app.run(debug=True)
